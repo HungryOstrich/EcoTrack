e38c08a8c38cf8672230b6f7e2c0eda276ef771e
kubab <kubab@DESKTOP-K0VM3J6>
2026-01-21 18:29:33 +0100

    baza danych

--- EcoTrack/Data/ApplicationDbContext.cs
+++ EcoTrack/Data/ApplicationDbContext.cs
@@ -5,16 +5,40 @@ using Microsoft.EntityFrameworkCore;
 
 namespace EcoTrack.Data
 {
-        public class ApplicationDbContext : IdentityDbContext<AppUser>
+    public class ApplicationDbContext : IdentityDbContext<AppUser>
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
         {
-            public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
-                : base(options)
-            {
-            }
-            public DbSet<EcoTrack.Models.ActivityLog> ActivityLog { get; set; } = default!;
-            public DbSet<EcoTrack.Models.EmissionFactor> EmissionFactor { get; set; } = default!;
-            public DbSet<EcoTrack.Models.EmissionSource> EmissionSource { get; set; } = default!;
-            public DbSet<EcoTrack.Models.Organization> Organization { get; set; } = default!;
         }
-    
+        public DbSet<Organization> Organizations { get; set; }
+        public DbSet<EmissionSource> EmissionSources { get; set; }
+        public DbSet<ActivityLog> ActivityLogs { get; set; }
+        public DbSet<EmissionFactor> EmissionFactors { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+
+          
+            builder.Entity<Organization>()
+                .HasMany(o => o.Users)
+                .WithMany(u => u.Organizations)
+                .UsingEntity(j => j.ToTable("OrganizationUsers"));
+
+           
+
+            builder.Entity<EmissionSource>()
+                .HasOne(e => e.Organization)
+                .WithMany(o => o.EmissionSources)
+                .HasForeignKey(e => e.OrganizationId)
+                .OnDelete(DeleteBehavior.Cascade); // Usunięcie organizacji usuwa źródła
+
+            builder.Entity<ActivityLog>()
+                .HasOne(a => a.EmissionSource)
+                .WithMany(e => e.ActivityLogs)
+                .HasForeignKey(a => a.EmissionSourceId)
+                .OnDelete(DeleteBehavior.Cascade); // Usunięcie źródła usuwa logi
+        }
+    }
 }
